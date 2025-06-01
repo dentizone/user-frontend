28c54dc716e8ffe0383eccc140f7cfde7a5af393
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-01 15:33:25 +0300

    Add Tailwind CSS and PrimeNG integration; disable Angular CLI analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
+import Aura from '@primeng/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        options: {
+          darkModeSelector: 'dark',
+        },
+        preset: Aura,
+      },
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
