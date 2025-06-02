1146b7ca9071dd05c28c356ca87727ded993cc6f
NourhaneAmir <92952125+NourhaneAmir@users.noreply.github.com>
2025-06-02 21:52:49 +0300

    Add Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
