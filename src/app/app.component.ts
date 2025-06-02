2301c00fd1458b3be5268f731e06bafb6bde536c
NourhaneAmir <92952125+NourhaneAmir@users.noreply.github.com>
2025-06-02 23:19:21 +0300

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule,NavBarComponent],
+  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
