32f0d1a3a670cf5f0905c511cb3b761ab04f8a74
Yara Nagy <yara1998@live.com>
2025-06-03 16:36:31 +0300

    authCard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ButtonModule],
+=======
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { RegisterComponent } from "./features/Auth/Register/Component/register/register.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, RegisterComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
