5b66ddc9bf02120e0f02ca20307a6a8da9269939
Nouran Yasser <nouranyasserr81@gmail.com>
2025-06-03 21:16:52 +0300

    verification page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import { LoginComponent } from "./features/loginPage/login/login.component";
+import { VerificationComponent } from "./features/verificationPage/verification/verification.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, LoginComponent],
+  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, LoginComponent, VerificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
