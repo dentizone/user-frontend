d3a76119d4fb7181d3c2e720b2211662f1e725d5
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-04 12:03:07 +0300

    Fixed Responsiveness in Navbar and Footer

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,23 +1,32 @@
+import { Component, HostListener } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
-opened=false;
-UserName="Nourhane"
-UserEmail="nourhane@gmail.com"
-opendropdown(){
-   this.opened = !this.opened;
-}
-@HostListener('document:click', ['$event'])
-onClickOutside(event: Event) {
-  const target = event.target as HTMLElement;
-  const inside = target.closest('#user-menu-button') || target.closest('#user-dropdown');
-  if (!inside) this.opened = false;
-}
+  opened = false;
+  mobileMenuOpened = false;
+  UserName = 'Nourhane';
+  UserEmail = 'nourhane@gmail.com';
+
+  opendropdown() {
+    this.opened = !this.opened;
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpened = !this.mobileMenuOpened;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    const inside =
+      target.closest('#user-menu-button') || target.closest('#user-dropdown');
+    if (!inside) this.opened = false;
+  }
 }
