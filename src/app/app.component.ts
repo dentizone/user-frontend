5ef842cd8e5a1aaaa4e30327e3423beaed85e21a
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-04 12:28:03 +0300

    Refactor Home page structure: replace HomeContainer with router outlet, remove unused components and styles, and update routing to use HomeContainerComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { HomeContainerComponent } from './features/Home/home-container.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
-import { FooterComponent } from "./shared/components/footer/footer.component";
-import { WelcomeCardComponent } from "./features/Home/AdsCard/welcome-card/welcome-card.component";
-import { ServiceSectionComponent } from './features/Home/ourServicesSection/service-section/service-section.component';
-import { AdsCardComponent } from "./features/Home/AdsCard/ads-card/ads-card.component";
-import { FeedbackSectionComponent } from "./features/Home/feedback-section/feedback-section.component";
-import { HomeContainerComponent } from "./features/Home/home-container/home-container.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, WelcomeCardComponent, ServiceSectionComponent, AdsCardComponent, FeedbackSectionComponent, HomeContainerComponent],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    NavBarComponent,
+    FooterComponent,
+    HomeContainerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { HomeContainerComponent } from './features/Home/home-container.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomeContainerComponent }];
