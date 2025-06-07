a9b3ad0913b0edd035528be7f1b09ff81f74ab62
NourhaneAmir <92952125+NourhaneAmir@users.noreply.github.com>
2025-06-07 04:21:42 +0300

    Add user posts

--- src/app/features/Profile/profile-container/profile-container.component.ts
+++ src/app/features/Profile/profile-container/profile-container.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { PersonalInfoComponent } from "../components/personal-info/personal-info.component";
+import { UserPostsComponent } from "../components/user-posts/user-posts.component";
 
 @Component({
+  standalone:true,
   selector: 'app-profile-container',
-  imports: [PersonalInfoComponent],
+  imports: [PersonalInfoComponent, UserPostsComponent],
   templateUrl: './profile-container.component.html',
   styleUrl: './profile-container.component.css'
 })
