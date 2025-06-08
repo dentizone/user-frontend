de14f1ac8251afb26d43af659a2121e72dab2e42
Nouran Yasser <nouranyasserr81@gmail.com>
2025-06-09 02:04:42 +0300

    categories page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { CategoriesComponent } from "./features/Categories/categories/categories.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, CategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/features/Categories/category.ts
@@ -0,0 +1,5 @@
+export interface Category {
+    name: string;
+    count: number;
+    icon: string;
+}
