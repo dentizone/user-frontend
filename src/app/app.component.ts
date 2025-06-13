71d1d8f932a797a665a23259a4f25abf58a9f7a0
Yara Nagy <yara1998@live.com>
2025-06-13 03:31:24 +0300

    added listing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { ForgetPasswordComponent } from './features/Auth/forget-password/forget-password.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { RouterOutlet } from '@angular/router';
+import { ListingPageComponent } from "./features/ListingPage/listing-page/listing-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, ListingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
