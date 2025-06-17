0fd1f6cc674c13ac54985df74fea64ad1ebe3382
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-17 12:39:15 +0300

    fix loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
+  standalone: true,
   imports: [],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.css'
+  styleUrls: ['./loader.component.css']
 })
 export class LoaderComponent {
 
