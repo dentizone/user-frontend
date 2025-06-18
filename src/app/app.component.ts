89bda21fc508ae0cf9cc4957f53641070e76d419
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-18 22:28:04 +0300

    Remove OrderComponent from app imports to streamline application structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
-import { OrderComponent } from "./features/Profile/order/order.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent, OrderComponent],
+  imports: [RouterOutlet, ButtonModule, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
