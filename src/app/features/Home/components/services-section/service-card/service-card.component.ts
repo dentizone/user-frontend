e99e065a24b6ce72daf4a89a84d41ccae929f271
NourhaneAmir <92952125+NourhaneAmir@users.noreply.github.com>
2025-06-20 02:14:42 +0300

    Fix Home page routing

--- src/app/features/Home/components/services-section/service-card/service-card.component.ts
+++ src/app/features/Home/components/services-section/service-card/service-card.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-card',
-  imports: [],
+  standalone:true,
+  imports: [RouterLink],
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.css'
 })
@@ -11,4 +13,9 @@ export class ServiceCardComponent {
 @Input() imageSrc="";
 @Input() title="";
 @Input() description=""
+
+  constructor(private readonly router: Router) {}
+  onclick(){
+    console.log(this.router.url +" clicked")
+  }
 }
