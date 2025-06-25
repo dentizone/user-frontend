a026df8e93cea0a6bd654f07236b68ce91fab3bf
Mahmoud Nasr <239.nasr@gmail.com>
2025-06-25 21:23:23 +0300

    Update fav-list.component.ts

--- src/app/features/FavList/fav-list/fav-list.component.ts
+++ src/app/features/FavList/fav-list/fav-list.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FavsService } from '../../favorites/favs.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-fav-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './fav-list.component.html',
   styleUrl: './fav-list.component.css',
 })
