0f52499dc9886218d624461479e8afbd830a50b5
Mahmoud Nasr <239.nasr@gmail.com>
2025-07-02 11:19:53 +0300

    Update profile.service.ts

--- src/app/features/Profile/service/profile.service.ts
+++ src/app/features/Profile/service/profile.service.ts
@@ -3,29 +3,31 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
-
- private apiUrl = 'https://apit.gitnasr.com/api/Users/me';
+  private apiUrl = 'https://apit.gitnasr.com/api/Users/me';
 
   constructor(private http: HttpClient) {}
 
   getUserProfile(): Observable<any> {
-    const token = localStorage.getItem('accessToken'); 
+    const token = localStorage.getItem('accessToken');
 
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
 
     return this.http.get<any>(this.apiUrl, { headers });
   }
-  getUserPosts():Observable<any>{
-    const token = localStorage.getItem('accessToken'); 
+  getUserPosts(): Observable<any> {
+    const token = localStorage.getItem('accessToken');
 
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
-    return this.http.get<any>(`https://apit.gitnasr.com/api/Posts/users/${token}/posts`, { headers });
+    return this.http.get<any>(
+      `https://apit.gitnasr.com/api/Posts/users/posts`,
+      { headers }
+    );
   }
 }
