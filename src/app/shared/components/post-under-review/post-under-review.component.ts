12790d3f804f268b41f2495042d684ebcb0fab06
Mahmoud Nasr <239.nasr@gmail.com>
2025-07-06 01:04:09 +0300

    Enhance PostUnderReviewComponent: update HTML structure and styling, and import CommonModule

--- src/app/shared/components/post-under-review/post-under-review.component.ts
+++ src/app/shared/components/post-under-review/post-under-review.component.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  
   selector: 'app-post-under-review',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-under-review.component.html',
   standalone: true
 })
